0622a88b208d9764965eae98bd6f5dbc7bcd3797
peterlabick <peterlabick@gmail.com>
2021-12-07 10:21:44 -0500

    Add skip login

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 set -e # Abort at first error
 echo "Running \"az login --service-principal -u "$1" -p "$3" --tenant "$2"\""
+eval "$4"
+
 #az login --service-principal -u "$1" -p "$3" --tenant "$2"
 echo "----------Logged In----------"
 eval "$4"
