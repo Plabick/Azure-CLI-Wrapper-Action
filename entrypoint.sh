5cffc6b655cd3f53f0ead5c769e68f2539c04136
Peter Labick <Plabick@users.noreply.github.com>
2021-12-07 10:46:46 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e # Abort at first error
-if[ "$5" != "true"]
+if[ ! "$5" == "true"]
 then
     echo "Running \"az login --service-principal -u "$1" -p "$3" --tenant "$2"\""
     az login --service-principal -u "$1" -p "$3" --tenant "$2"
